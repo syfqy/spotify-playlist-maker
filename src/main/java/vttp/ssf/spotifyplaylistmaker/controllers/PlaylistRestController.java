589e772ec91e3c8c98e738047a371e9a421a097b
syfqy <syafiq.ysf@gmail.com>
2022-08-10 18:51:02 +0800

    feat: add redis configs

---
+++ src/main/java/vttp/ssf/spotifyplaylistmaker/controllers/PlaylistRestController.java
@@ -0,0 +1,10 @@
+package vttp.ssf.spotifyplaylistmaker.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/playlists")
+public class PlaylistRestController {
+    
+}
