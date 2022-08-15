161943261060c66dc0ad5eecdf59ce251d8ecc0d
syfqy <syafiq.ysf@gmail.com>
2022-08-15 19:45:23 +0800

    feat: add navbar and main on home page

--- src/main/java/vttp/ssf/spotifyplaylistmaker/controllers/PlaylistMakerRestController.java
+++ src/main/java/vttp/ssf/spotifyplaylistmaker/controllers/PlaylistMakerRestController.java
@@ -1,7 +1,6 @@
 package vttp.ssf.spotifyplaylistmaker.controllers;
 
 import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import se.michaelthelin.spotify.model_objects.specification.Track;
 import vttp.ssf.spotifyplaylistmaker.services.PlaylistMakerService;
 
@@ -18,20 +16,32 @@ import vttp.ssf.spotifyplaylistmaker.services.PlaylistMakerService;
 @RequestMapping("/api")
 public class PlaylistMakerRestController {
 
-    @Autowired
-    PlaylistMakerService plmService;
+  @Autowired
+  PlaylistMakerService plmService;
 
-    private static final Logger logger = LoggerFactory.getLogger(PlaylistMakerRestController.class);
+  private static final Logger logger = LoggerFactory.getLogger(
+    PlaylistMakerRestController.class
+  );
 
-    @GetMapping("/search-playlists")
-    public ResponseEntity<List<Track>> searchPlaylists(@RequestParam(name="keyword") String keyword,
-            @RequestParam("playlists") int nPlaylists, @RequestParam(name="tracks") int nTracks) {
-
-        logger.info("Generating playlist of {} tracks via mining {} playlists about: {}", nTracks, nPlaylists, keyword);
-        List<Track> topTracksList = plmService.getTopTracksOfKeyword(keyword, nTracks, nPlaylists);
-        return ResponseEntity.ok(topTracksList);
-    }
-
-    // TODO: save playlist to spotify acc
+  @GetMapping("/search-playlists")
+  public ResponseEntity<List<Track>> searchPlaylists(
+    @RequestParam(name = "keyword") String keyword,
+    @RequestParam("playlists") int nPlaylists,
+    @RequestParam(name = "tracks") int nTracks
+  ) {
+    logger.info(
+      "Generating playlist of {} tracks via mining {} playlists about: {}",
+      nTracks,
+      nPlaylists,
+      keyword
+    );
+    List<Track> topTracksList = plmService.getTopTracksOfKeyword(
+      keyword,
+      nTracks,
+      nPlaylists
+    );
+    return ResponseEntity.ok(topTracksList);
+  }
+  // TODO: save playlist to redis
 
 }
